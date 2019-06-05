36363a187b42b98a9130a797d869f46f0a859027
xueshufeng <89688209@qq.com>
2019-06-05 23:16:44 +0800

    python2 版本穿透式 gateway，   ctpltsgateway

---
+++ vnpy/trader/gateway/ctpltsGateway/__init__.py
@@ -0,0 +1,10 @@
+# encoding: UTF-8
+
+from vnpy.trader import vtConstant
+from ctpltsGateway import CtpLTSGateway
+
+gatewayClass = CtpLTSGateway
+gatewayName = 'CTP'
+gatewayDisplayName = 'CTP'
+gatewayType = vtConstant.GATEWAYTYPE_FUTURES
+gatewayQryEnabled = True
